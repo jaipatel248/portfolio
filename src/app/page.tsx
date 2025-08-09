9d02fc409e80c37699e5d239f17de819fe96370a
Jay Patel <55126933+jaipatel248@users.noreply.github.com>
2025-08-09 13:50:39 +0000

    MYPOR-4 Hide Recommendations and blogs section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import PostSection from "@/components/sections/blog/PostSection";
 import WorkExperienceSection from "@/components/sections/experience/WorkExperienceSection";
 import HeroSection from "@/components/sections/hero/HeroSection";
 import Projects from "@/components/sections/projects/Projects";
-import RecommendationsSection from "@/components/sections/recommendations/RecommendationsSection";
 import SkillsSection from "@/components/sections/skills/SkillsSection";
 import { Button } from "@/components/ui";
 import ClickSpark from "@/components/ui/ClickSpark";
@@ -14,69 +12,58 @@ import { ScrollEffect } from "@/lib/animations";
 import Link from "next/link";
 import { MdEmail } from "react-icons/md";
 
-
 export default function HomePage() {
   return (
-    <div className="container mx-auto">
-
+    <div className='container mx-auto'>
       <HeroSection />
 
-
-      <div className="py-4 border-t mt-6">
+      <div className='py-4 border-t mt-6'>
         <SkillsSection />
       </div>
 
-      <div id="work" className="py-4 border-t mt-6">
+      <div id='work' className='py-4 border-t mt-6'>
         <WorkExperienceSection />
       </div>
 
-      <div id="projects" className="py-4 border-t mt-6">
+      <div id='projects' className='py-4 border-t mt-6'>
         <Projects />
       </div>
 
-
-      <div className="py-4 border-t mt-6">
+      {/* <div className="py-4 border-t mt-6">
         <RecommendationsSection />
       </div>
 
       <div className="py-4 border-t mt-6">
         <PostSection />
-      </div>
+      </div> */}
 
       <ClickSpark
-        sparkColor="var(--headline)"
+        sparkColor='var(--headline)'
         sparkSize={10}
         sparkRadius={15}
         sparkCount={8}
         duration={400}
       >
-        <ScrollEffect type="fadeIn">
-          <div className="mt-16 mb-12 text-center w-full mx-auto">
-            <div className="p-8 rounded-2xl bg-[var(--card-background)] border border-[var(--card-border-color)] shadow-lg">
+        <ScrollEffect type='fadeIn'>
+          <div className='mt-16 mb-12 text-center w-full mx-auto'>
+            <div className='p-8 rounded-2xl bg-[var(--card-background)] border border-[var(--card-border-color)] shadow-lg'>
               <ShinyText
                 text="Let's Work Together"
                 disabled={false}
                 speed={3}
-                className="text-2xl md:text-3xl font-bold mb-4 "
+                className='text-2xl md:text-3xl font-bold mb-4 '
               />
-              <p className="text-[var(--paragraph)] mb-6 max-w-2xl mx-auto">
+              <p className='text-[var(--paragraph)] mb-6 max-w-2xl mx-auto'>
                 I'm open to discussing new projects, creative ideas, or
                 opportunities to be part of your vision.
               </p>
-              <Link
-                href={mailto}
-           
-           
-
->
-
-<Button icon={<MdEmail className="h-4 w-4"/>} className="mx-auto flex-row-reverse">
-                Get in Touch
-
-
-</Button>
-
-             
+              <Link href={mailto}>
+                <Button
+                  icon={<MdEmail className='h-4 w-4' />}
+                  className='mx-auto flex-row-reverse'
+                >
+                  Get in Touch
+                </Button>
               </Link>
             </div>
           </div>
